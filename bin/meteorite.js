46e73e402d0dd2fde13d59d0be8d4361d54acdfa
Mike Bannister <notimpossiblemike@gmail.com>
2012-06-26 18:41:43 -0400

    renamed installer to project and broke out installer() from run()

--- bin/meteorite.js
+++ bin/meteorite.js
@@ -1,24 +1,12 @@
 #!/usr/bin/env node
 
-var Installer = require('../lib/installer');
+var Project = require('../lib/project');
 var Runner = require('../lib/runner');
 var argv = require('optimist').argv;
 
 var showUsage = function() {
   console.log('show usage');
   process.exit();
-};
-
-var installer = function() {
-  return new Installer().run(function() {
-    console.log('Installation complete!');
-  });
-};
-
-var runner = function() {
-  return new Runner().run(function() {
-    console.log('Running meteor!');
-  });
 };
 
 var showHome = function() {
@@ -29,12 +17,15 @@ var command = argv._[0] || 'run';
 switch(command) {
 
   case 'run':
-    installer();
-    runner();
+    project = new Project().run(function() {
+      console.log('Project running!');
+    });
     break;
 
   case 'install':
-    installer();
+    project = new Project().install(function() {
+      console.log('Project installed!');
+    });
     break;
 
   case 'home':
