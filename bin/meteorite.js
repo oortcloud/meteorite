2001f4f5da10e1fa9cac5a3b5d0dff352b044c3b
Mike Bannister <notimpossiblemike@gmail.com>
2012-06-26 01:57:29 -0400

    basic command parsing

--- bin/meteorite.js
+++ bin/meteorite.js
@@ -1,7 +1,46 @@
 #!/usr/bin/env node
 
 var Installer = require('../lib/installer');
+var Runner = require('../lib/runner');
+var argv = require('optimist').argv;
 
-new Installer().run(function() {
-  console.log('Intallation complete!');
-});
+var showUsage = function() {
+  console.log('show usage');
+  process.exit();
+};
+
+var install = function() {
+  new Installer().run(function() {
+    console.log('Installation complete!');
+  });
+};
+
+var run = function() {
+  new Runner().run(function() {
+    console.log('Running meteor!');
+  });
+};
+
+var showHome = function() {
+};
+
+var command = argv._[0] || 'run';
+
+switch(command) {
+
+  case 'run':
+    install();
+    run();
+    break;
+
+  case 'install':
+    install();
+    break;
+
+  case 'home':
+    showHome();
+    break;
+
+  default:
+    showUsage();
+}
