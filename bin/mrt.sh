9ca25b8b5e57f6fa1dd077e3ddecacc3863b65ef
Tom Coleman <tom@thesnail.org>
2012-06-30 18:35:14 +1000

    Re-arranged so the project takes responsibility for creating the .meteor/meteorite directory.
    Also added creation of gitignore

--- bin/mrt.sh
+++ bin/mrt.sh
@@ -2,17 +2,17 @@
 
 BASEDIR=$(dirname "$0")
 MRTJS="$BASEDIR/mrt.js"
+MRT_DIR="./.meteor/meteorite"
 
 $MRTJS $@:1
 SUB_COMMAND=${@:1:1}
 
 if [[ $SUB_COMMAND != "install" ]]
 then
-
   if [[ $SUB_COMMAND == "create" ]]
   then
     echo "Error: create not yet supported"
   else
-    bash ./.meteor/meteorite/meteor/meteor $@
+    bash $MRT_DIR/meteor $@
   fi
 fi
