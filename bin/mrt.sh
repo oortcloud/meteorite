b0770a8ad50ae03423f985b6e119dff6e04adaa3
Mike Bannister <notimpossiblemike@gmail.com>
2012-06-30 02:41:17 -0400

    cleanup mrt.sh

--- bin/mrt.sh
+++ bin/mrt.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-is_supported_subcommand() {
-  local supported=( "run" "deploy" "update" "add" "list" "remove" "bundle" "reset" "installed" )
-  local command=$1
-  shift
-  for hay; do
-      [[ $supported == $command ]] && return 0
-  done
-  return 1
-}
-
 BASEDIR=$(dirname "$0")
 MRTJS="$BASEDIR/mrt.js"
 
 $MRTJS $@:1
 SUB_COMMAND=${@:1:1}
-
-if ! is_supported_subcommand $SUB_COMMAND
-then
-  SUB_COMMAND=run
-fi
 
 if [[ $SUB_COMMAND != "install" ]]
 then
