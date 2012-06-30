e95581d697f8f03dd9f16ed1e367420694d0f317
Mike Bannister <notimpossiblemike@gmail.com>
2012-06-30 15:50:39 -0400

    comments/cleanup

--- bin/mrt.js
+++ bin/mrt.js
@@ -5,37 +5,46 @@ var argv = require('optimist').argv;
 var path = require('path');
 var fs = require('fs');
 
+// Figure out which command the user is 
+// running, user 'run' if none specified
 var command = argv._[0] || 'run';
 
 if (command === 'uninstall') {
 
+  // Uninstall project
   new Project().uninstall();
 
 } else if (command === 'create') {
 
+  // What's the name of the new app?
   var appName = argv._[1];
-  if (appName) {
-    new Meteor({
-      branch: 'master'
-    }).repo.fetch(function() {});
-  }
+
+  // Get ready for `meteor create` by checking out
+  // a copy of meteor
+  // TODO allow specifying fork/branch/tag/ref/etc
+  if (appName)
+    new Meteor({ branch: 'master' }).repo.fetch(function() {});
 
 } else if (command === 'post_create') {
 
+  // Figure out paths
   var appName = argv._[2];
   var appPath = path.join(process.cwd(), appName);
   var smartJsonPath = path.join(appPath, 'smart.json');
 
+  // Make a default smart.json for the new project
   fs.writeFile(smartJsonPath, '{"packages": {}}', function(err) {
     if(err) {
       console.log("Error: could not create smart.json", err);
     }
   });
 
+  // Output path to new project, used by `mrt.js`
   console.log(appPath);
 
 } else {
 
+  // Just install the project, nothing else!
   new Project().install(function() {});
 
 }
--- bin/mrt.sh
+++ bin/mrt.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
+# Figure out which subcommand is being run
 SUB_COMMAND=${@:1:1}
+
+# Figure out paths
 BASEDIR=$(dirname "$0")
 MRTJS="$BASEDIR/mrt.js"
 MRT_DIR="./.meteor/meteorite"
 
+# Run mrt with given arguments
 $MRTJS $@
 
+# If we're installing or uninstalling she show is over
 if [[ $SUB_COMMAND != "install" && $SUB_COMMAND != "uninstall" ]]
 then
 
+  # Do special stuff when creating a new project
   if [[ $SUB_COMMAND == "create" ]]
   then
 
-  bash $HOME/.meteorite/meteors/branch/master/meteor $@
-  APP_PATH=`$MRTJS post_create $@`
-  cd $APP_PATH && $MRTJS install
+    # Let meteor create the proj
+    bash $HOME/.meteorite/meteors/branch/master/meteor $@
+
+    # Let mrt.js do it's post create trix
+    APP_PATH=`$MRTJS post_create $@`
+  
+    # Install meteorite in new proj
+    cd $APP_PATH && $MRTJS install
 
   else
 
-    echo bash $MRT_DIR/meteor $@
+    # Go ahead and run meteor
     bash $MRT_DIR/meteor $@
 
   fi
