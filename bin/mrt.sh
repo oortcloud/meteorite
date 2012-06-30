2ca8244e9f4d1d7e00ec80292c85fea2576bef2a
Mike Bannister <notimpossiblemike@gmail.com>
2012-06-30 01:02:20 -0400

    cleanup

--- bin/mrt.sh
+++ bin/mrt.sh
@@ -4,11 +4,9 @@ BASEDIR=$(dirname "$0")
 MRTJS="$BASEDIR/mrt.js"
 
 $MRTJS $@:1
-
 COMMAND=${@:1:1}
 
 if [[ $COMMAND != "install" ]]
 then
-  echo $@
   bash ./.meteor/meteorite/meteor/meteor $@
 fi
