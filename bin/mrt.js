e0559d6609a7536f61690d02d946310a9ea135c5
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-02 14:27:07 -0400

    Revert "don't use process.argv directly"
    This reverts commit b9800ff233e90c8a9776dc2ebee7cf5c5e6fd274.

--- bin/mrt.js
+++ bin/mrt.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 
 var _ = require('underscore');
-var args = require('optimist').argv;
+
 var Meteorite = require('../lib/meteorite');
+var args = require('optimist').argv;
+
+// Figure out which subcommand the user is 
+// running, use 'run' if none specified
+var subCommandName = args._[0] || 'run';
 
 // Build a Meteorite instance
-var meteorite = new Meteorite(args).start(_.identity);
+var meteorite = new Meteorite();
+
+// Run user's subcommand
+meteorite[subCommandName](_.identity);
