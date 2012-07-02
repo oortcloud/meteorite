b9800ff233e90c8a9776dc2ebee7cf5c5e6fd274
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-02 14:03:06 -0400

    don't use process.argv directly

--- bin/mrt.js
+++ bin/mrt.js
@@ -1,16 +1,8 @@
 #!/usr/bin/env node
 
 var _ = require('underscore');
-
-var Meteorite = require('../lib/meteorite');
 var args = require('optimist').argv;
-
-// Figure out which subcommand the user is 
-// running, use 'run' if none specified
-var subCommandName = args._[0] || 'run';
+var Meteorite = require('../lib/meteorite');
 
 // Build a Meteorite instance
-var meteorite = new Meteorite();
-
-// Run user's subcommand
-meteorite[subCommandName](_.identity);
+var meteorite = new Meteorite(args).start(_.identity);
