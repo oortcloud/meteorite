4abad58e0add06b0b1bfe85653c26edacc67ccd4
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-05 22:04:59 -0400

    oops forgot file

---
+++ spec/acceptance/install_spec.js
@@ -0,0 +1,23 @@
+var mrt = require('./helpers');
+
+
+///// NOTE ///////
+// All real install tests are done on run because install is just a subset of run but run gives us simpler ways of verifying behavior.
+
+describe('`mrt install`', function() {
+  
+  beforeEach(function(done) {
+    mrt.cleanup(done);
+  });
+
+  // Just a superficial test to make sure install is working
+  it("should install meteor and the app's smart pacakge", function(done) {
+    mrt.invoke('run', 'app-with-smart-pkg', {
+      waitForOutput: [
+        "Fetching package mrt-test-pkg1 (branch: master)",
+        "Fetching Meteor (branch: master)"
+      ]
+    }, done);
+  })
+  
+});
