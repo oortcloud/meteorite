5dc41007f45620b3eaf55258cc15a176ca5f8197
Tom Coleman <tom@thesnail.org>
2012-07-15 16:24:57 +1000

    Unit tests are now working
    (I'm not sure the commits are being compared properly though).

--- spec/unit/package_mock.js
+++ spec/unit/package_mock.js
@@ -6,10 +6,16 @@ var PKG_DEPENDENCIES = {
   }}
 }
 
+var PKG_COMMITS = {
+  'mrt-test-pkg1': '3ab9c811313dbedc48269f39a78b86617653fa91',
+  'mrt-test-pkg2': '223e2266b1c593abcdeaace1d5b0955b372e0f34'
+}
+
 Package.prototype.readDependencies = function(fn) {
   
   // rather than downloading the package and reading the smart.json,
   // we are just going to fudge it from the above array
   this.dependencies = new Dependencies(this.source.path, PKG_DEPENDENCIES[this.name]);
+  this.source.commit = PKG_COMMITS[this.name];
   fn()
 }
