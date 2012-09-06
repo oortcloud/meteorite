8ffae3e98ad455237c945140e81fd68ff4860396
Mike Bannister <notimpossiblemike@gmail.com>
2012-09-06 04:36:50 -0400

    messing with output

--- bin/mrt.js
+++ bin/mrt.js
@@ -14,8 +14,13 @@ var subCommandName = meteoriteArgs._[0] || 'run';
 var meteorArgs = process.argv.slice(2);
 var meteorite = new Meteorite(meteoriteArgs, meteorArgs);
 
-if (meteoriteArgs.version)
+if (meteoriteArgs.version) {
+  console.blank();
+  console.header();
   console.log('Meteorite version ' + require('../package').version);
+  console.footer();
+  console.blank();
+}
 
 if (!meteorite[subCommandName])
   throw "Subcommand " + subCommandName + " does not exist!";
