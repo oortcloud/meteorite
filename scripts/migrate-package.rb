189125bf50a51e310fc014c28d6bbf126b7e73f1
Tom Coleman <tom@thesnail.org>
2014-07-25 20:11:06 +1000

    Added a migrate-package script which we’ll use to do the migration.

---
+++ scripts/migrate-package.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/env ruby
+
+METEOR_EXECUTABLE = "~/Development/Meteor/meteor/meteor"
+ATMOSPHERE_HOST = 'localhost'
+ATMOSPHERE_PORT = 3000
+TROPOSPHERE_HOST = 'packages-atmosphere.meteor.com'
+DEFAULT_USERNAME = 'mrt'
+
+require 'json'
+
+def run_and_check(command)
+  output = `#{command}`
+  if ($? != 0)
+    raise "Command #{command} failed:\n" + output
+  end
+end
+
+# check arguments
+dir = ARGV[0]
+username = ARGV[1]
+
+# 1. check usage
+if (not dir or not username)
+  raise "Usage: ruby migrate-package.rb dir md-username "
+end
+
+# 2. figure out basic package details
+smart_json = JSON.parse(File.read(File.join(dir, 'smart.json')))
+package_name = smart_json["name"]
+version_name = smart_json["version"]
+raise "Package name not found -- is smart.json OK?" unless package_name
+raise "Version not found -- is smart.json OK?" unless version_name
+
+# 3. check if package is already registered with our ATMOS host
+command = "ddp --host #{ATMOSPHERE_HOST} --port #{ATMOSPHERE_PORT} subscribe package #{package_name}"
+data = JSON.parse(`#{command}`)
+if ($? != 0 or not data["packages"].length)
+  raise "Problem finding package '#{package_name}' on #{ATMOSPHERE_HOST}:#{ATMOSPHERE_PORT}. Are you sure it's there?"
+end
+
+package = data["packages"].values[0]
+raise "Something wrong with package data" unless (package["name"] === package_name)
+
+version = package["versions"].find {|v| v["version"] == version_name}
+raise "Couldn't find package version #{version} on #{ATMOSPHERE_HOST}:#{ATMOSPHERE_PORT}" unless version
+
+if version["troposphereIdentifier"]
+  raise "Package already registered as #{version["troposphereIdentifier"]}"
+end
+
+# 4. run `mrt migrate-package` 
+troposphere_name = "#{username}:#{package_name}"
+run_and_check "mrt --repoHost #{ATMOSPHERE_HOST} --repoPort #{ATMOSPHERE_PORT} migrate-package #{dir} #{username}"
+
+unless Dir.exists?(troposphere_name)
+  raise "Package Migration Failed to create #{troposphere_name}"
+end
+
+# 5. publish to troposphere
+Dir.chdir troposphere_name
+begin # ugh. not sure there's a better way
+  run_and_check "#{METEOR_EXECUTABLE} publish --create"
+rescue
+  run_and_check "#{METEOR_EXECUTABLE} publish"
+end
+
+if username != DEFAULT_USERNAME
+  run_and_check "#{METEOR_EXECUTABLE} admin maintainers #{troposphere_name} --add #{username}"
+end
+
+# XXX: this doesn't seem to work
+# if package["homepage"]
+#   run_and_check "#{METEOR_EXECUTABLE} admin set-homepage #{troposphere_name} #{package["homepage"]}"
+# end
+
+# 6. inform atmosphere
+run_and_check "ddp --host #{ATMOSPHERE_HOST} --port #{ATMOSPHERE_PORT} call " +
+  "markTroposphereIdentifier #{package_name} #{version_name} #{troposphere_name}@#{version_name}"
