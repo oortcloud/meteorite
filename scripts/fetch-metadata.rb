0474652c7dda464143720a792c372ad4c71bb1a8
Tom Coleman <tom@thesnail.org>
2014-07-28 15:33:53 +1000

    Added more scripts to take us closer

---
+++ scripts/fetch-metadata.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+ATMOSPHERE_HOST = 'localhost'
+ATMOSPHERE_PORT = 3000
+
+MONGO_HOST = 'localhost'
+MONGO_PORT = 3011
+MONGO_DB = 'meteor'
+MONGO_COLLECTION = 'versions'
+
+require 'json'
+require 'mongo'
+include Mongo
+
+mongo_client = MongoClient.new(MONGO_HOST, MONGO_PORT)
+versions = mongo_client[MONGO_DB][MONGO_COLLECTION]
+
+# first grab data from atmosphere, and write to mongo
+command = "ddp --host #{ATMOSPHERE_HOST} --port #{ATMOSPHERE_PORT} subscribe packages"
+packages_metadata = JSON.parse(`#{command}`)
+
+command = "ddp --host #{ATMOSPHERE_HOST} --port #{ATMOSPHERE_PORT} subscribe usernames"
+usernames_metadata = JSON.parse(`#{command}`)
+
+packages_metadata['packages'].each do |id, pkg|
+  username = nil
+  
+  [pkg['userId']].concat(pkg['userIds'] || []).any? do |user_id|
+    user_metadata = usernames_metadata['users'][user_id]
+    if (user_metadata and user_metadata['services']['meteor-developer'])
+      username = user_metadata['services']['meteor-developer']['username']
+    end
+  end
+  
+  pkg['versions'].each do |version|
+    begin # uggg, upsert-ish
+      versions.insert(
+        name: pkg['name'],
+        version: version['version'],
+        username: username,
+        git: version['git']
+      )
+    rescue
+      nil
+    end
+  end
+end
