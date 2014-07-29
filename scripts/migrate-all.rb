3f42155db6ec5f888a275a457c7606fb5b28e13a
Tom Coleman <tom@thesnail.org>
2014-07-29 19:45:13 +1000

    Some improvements

--- scripts/migrate-all.rb
+++ scripts/migrate-all.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+PROCESSES=4
+
 MIGRATION_SCRIPT = File.join(File.dirname(__FILE__), 'migrate-package.rb')
 DEFAULT_USERNAME = 'mrt'
 
@@ -7,24 +9,27 @@ MONGO_HOST = 'localhost'
 MONGO_PORT = 3011
 MONGO_DB = 'meteor'
 MONGO_COLLECTION = 'versions'
-
 require 'mongo'
 include Mongo
 
 mongo_client = MongoClient.new(MONGO_HOST, MONGO_PORT)
 $versions = mongo_client[MONGO_DB][MONGO_COLLECTION]
 
+require 'open3'
 def run_and_report(command, version)
-  output = `#{command} 2>&1`
-  if ($? != 0)
-    $versions.update({_id: version['_id']}, {'$set' => {error: output}})
+  stdout, stderr, status = Open3.capture3(command)
+  if (status != 0)
+    error = stderr + "\n" + stdout
+    $versions.update({_id: version['_id']}, {'$set' => {error: error}})
     return false
   end
   return true
 end
 
+require 'parallel'
 
-$versions.find({complete: {'$exists' => false}}).each do |version|
+versions = $versions.find({complete: {'$exists' => false}}).to_a
+Parallel.each(versions) do |version|
   # 1. clone the package into the right directory
   dir_name = File.join(version['name'], version['version'])
   
@@ -40,10 +45,12 @@ $versions.find({complete: {'$exists' => false}}).each do |version|
   end
   
   # 2. call ./migrate-package
+  puts "> Migrating #{dir_name}"
   command = "#{MIGRATION_SCRIPT} #{dir_name} #{version['username'] || DEFAULT_USERNAME}"
   next unless run_and_report(command, version)
   
   # 3. register success!
+  puts ">> Success! for #{dir_name}"
   $versions.update({_id: version['_id']}, {
     '$set' => {complete: true},
     '$unset' => {error: 1}
